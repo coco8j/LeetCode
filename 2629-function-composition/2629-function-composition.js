b9e17bfff44597c153da487f34417a6f104a9de4
coco8j <160463756+coco8j@users.noreply.github.com>
2024-05-07 22:20:33 +0900

    Time: 60 ms (76.5%), Space: 50.2 MB (30.44%) - LeetHub

---
+++ 2629-function-composition/2629-function-composition.js
@@ -0,0 +1,21 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function (functions) {
+
+    return function (x) {
+        let result = x;
+        const index = functions.length - 1;
+
+        for (let i = index; i >= 0; i--) {
+            result = functions[i](result);
+        }
+        return result;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
