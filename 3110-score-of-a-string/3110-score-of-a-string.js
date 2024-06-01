58ecbbeffffd8ede13d6262cb0665549b7b43dcb
coco8j <160463756+coco8j@users.noreply.github.com>
2024-06-01 11:13:27 +0900

    Time: 55 ms (82.28%), Space: 50.9 MB (24.62%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.js
+++ 3110-score-of-a-string/3110-score-of-a-string.js
@@ -2,18 +2,18 @@
  * @param {string} s
  * @return {number}
  */
-var scoreOfString = function(s) {
+var scoreOfString = function (s) {
     const sArray = [...s];
-    let result = 0; 
+    let result = 0;
 
     for (let i = 0; i < sArray.length - 1; i++) {
         result = result + imsub(sArray[i], sArray[i + 1]);
     }
-    
+
     return result;
 };
 
-function imsub (a, b) {
+function imsub(a, b) {
     const aASCII = a.charCodeAt();
     const bASCII = b.charCodeAt();
     return Math.abs(aASCII - bASCII);
