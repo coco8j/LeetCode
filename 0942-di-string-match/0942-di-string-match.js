f2fa3e5ab846a05fc138503e51cba4578f490410
JuYeon <coco8j.lee@gmail.com>
2024-11-02 23:40:32 +0900

    Time: 2 ms (82%), Space: 53.2 MB (57.78%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number[]}
+ */
+/**
+ * @param {string} s
+ * @return {number[]}
+ */
+var diStringMatch = function (s) {
+  let count1 = -1;
+  let count2 = s.length + 1;
+  const result = [];
+  for (let i = 0; i <= s.length; i++) {
+    if (s[i] === "I") {
+      count1 = count1 + 1;
+      result.push(count1);
+    } else {
+      count2 = count2 - 1;
+      result.push(count2);
+    }
+  }
+  return result;
+};
