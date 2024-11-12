650d24581440237b0343b6311ab7e11db5e25c01
JuYeon <coco8j.lee@gmail.com>
2024-11-13 03:19:47 +0900

    Time: 162 ms (50%), Space: 91.6 MB (14.29%) - LeetHub

--- 0100-most-beautiful-item-for-each-query/0100-most-beautiful-item-for-each-query.js
+++ 0100-most-beautiful-item-for-each-query/0100-most-beautiful-item-for-each-query.js
@@ -3,27 +3,31 @@
  * @param {number[]} queries
  * @return {number[]}
  */
-/**
- * @param {number[][]} items
- * @param {number[]} queries
- * @return {number[]}
- */
 var maximumBeauty = function (items, queries) {
     items.sort((a, b) => a[0] - b[0]);
-    let mostBeautys = [];
 
-    for (let i = 0; i < queries.length; i++) {
-        const priceBound = queries[i];
-        let mostBeauty = 0;
+    const uniqueItems = [];
+    let mostBeautys = 0;
 
-        for (let j = 0; j < items.length && items[j][0] <= priceBound; j++) {
-            const [price, beauty] = items[j];
-            if (mostBeauty < beauty) mostBeauty = beauty;
-        }
-
-        mostBeautys[i] = mostBeauty;
-
+    for (const [price, beauty] of items) {
+        mostBeautys = Math.max(mostBeautys, beauty);
+        uniqueItems.push([price, mostBeautys]);
     }
 
-    return mostBeautys;
+    return queries.map((priceBound) => {
+        let left = 0;
+        let right = uniqueItems.length - 1;
+
+        while (left <= right) {
+            const mid = Math.floor((right + left) / 2);
+            const midPrice = uniqueItems[mid][0];
+
+            if (midPrice <= priceBound) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return right >= 0 ? uniqueItems[right][1] : 0;
+    })
 };
